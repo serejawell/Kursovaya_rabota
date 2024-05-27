0afc1282cf2870811dd15eb3f99439cc25c64b24
Sergey Petrov <sev2316@yandex.ru>
2024-05-27 23:27:39 +0300

    adding funcs in utils

--- utils.py
+++ utils.py
@@ -1,4 +1,5 @@
 from class_operation import Currency, OperationAmount, Operation
+from datetime import datetime
 import json
 
 
@@ -44,12 +45,37 @@ def get_executed_operations(operations_list):
     return executed_list
 
 
+def formation_datetime(date):
+    """Метод для форматирования времени из нашего файла json в требуемый"""
+    date_object = datetime.fromisoformat(date)
+    str_dt_obj = date_object.strftime('%d.%m.%y')
+    return str_dt_obj
+
+def card_number_split_and_hide(card_infromation):
+    """В этом методе мы разделяем наш счет на название карты и номер для удобной работы с ними, в
+    последующих действиях этот метот можно будет применить к from и to. Также мы не трогаем счет, и закрываем
+    нужное кол-во цифр звездочками для секретности"""
+    if "Счет" in card_infromation:
+        return "Счет **{}".format(card_infromation[-4:])
+    else:
+        card_info = card_infromation.split(" ")
+        card_name = []
+        for card in card_info:
+            if card.isalpha():
+                card_name.append(card)
+            elif card.isdigit():
+                card_number = " ".join([card[i:i + 4] for i in range(0, len(card), 4)])
+                card_number = "{}** **** {}".format(card_number[:7], card_number[-4:])
+                card_name.append(card_number)
+        return " ".join(card_name)
+
+
 def get_five_last_executed_operations(operations):
     """Выводит последние 5 успешных операций"""
     for operation in operations[0:5]:
-        print(f"""
-        {operation.formation_datetime()} {operation.description}
-        {operation.card_number_split_and_hide(operation.by)} -> {operation.card_number_split_and_hide(operation.to)}
+        print(f"""{formation_datetime(operation.date)} {operation.description}
+        {card_number_split_and_hide(operation.by)} -> {card_number_split_and_hide(operation.to)}
         {operation.operation_amount.amount} {operation.operation_amount.currency.name}""")
+        print("")
 
 
