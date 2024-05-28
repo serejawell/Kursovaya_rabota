c2d246ec4f7361b85ccda0d4321c1312d1e5b183
Sergey Petrov <sev2316@yandex.ru>
2024-05-28 23:39:07 +0300

    reloaded main.py with new work

--- main.py
+++ main.py
@@ -1,9 +1,19 @@
-from utils import get_class_to_ops_and_sort, get_data, get_executed_operations, get_five_last_executed_operations
+from utils import get_data, get_executing_data, get_format_date, card_number_split_and_hide, get_last_five_operations, get_amount_and_currency
 
 def main():
-    operations = get_class_to_ops_and_sort(get_data())
-    executed_operations = get_executed_operations(operations)
-    get_five_last_executed_operations(executed_operations)
+    data = get_data()
+    executed_data = get_executing_data(data)
+    operations = get_last_five_operations(executed_data)
+    for operation in operations:
+        date = get_format_date(operation.get('date'))
+        description = operation.get('description')
+        by = card_number_split_and_hide(operation.setdefault("from", ""))
+        to = card_number_split_and_hide(operation.setdefault('to', ""))
+        currency = get_amount_and_currency(operation)
+        print(f"""  {date} {description}
+        {by} -> {to}
+        {currency} \n""")
+
 
 
 if __name__ == '__main__':
