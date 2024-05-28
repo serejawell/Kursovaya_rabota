25429415c13cb887869290d3eb1dbe9144d6c2d0
Sergey Petrov <sev2316@yandex.ru>
2024-05-28 23:41:46 +0300

    reworked all

--- utils.py
+++ utils.py
@@ -1,6 +1,5 @@
-from class_operation import Currency, OperationAmount, Operation
-from datetime import datetime
 import json
+from datetime import datetime
 
 
 def get_data():
@@ -9,56 +8,30 @@ def get_data():
         data = json.load(f)
         return data
 
-def get_class_to_ops_and_sort(data):
-    """Согласно полям класса распределяем все данные"""
-    operations = []
-    for item in data:
-        currency = Currency(
-            name=item.get("operationAmount", {}).get("currency", {}).setdefault("name", ""),
-            code=item.get("operationAmount", {}).get("currency", {}).setdefault("code", "")
-        )
-        operation_amount = OperationAmount(
-            amount=item.get("operationAmount", {}).setdefault("amount", ""),
-            currency=currency
-        )
 
-        operation_main = Operation(
-            id=item.setdefault("id", ""),
-            state=item.setdefault("state", ""),
-            date=item.setdefault("date", ""),
-            operation_amount=operation_amount,
-            description=item.setdefault("description", ""),
-            by=item.setdefault("from", ""),
-            to=item.setdefault("to", "")
-        )
-        operations.append(operation_main)
-    sorted_operations = sorted(operations, key=lambda x: x.date, reverse=True)
-    return sorted_operations
+def get_executing_data(data):
+    """Забираем успешные операции, с помощью datetime преобразуем дату"""
+    new_operations_list = []
+    for operations in data:
+        if operations and operations.get('date') is not None and operations.get('state') == 'EXECUTED':
+            operations['date'] = datetime.fromisoformat(operations['date'])
+            new_operations_list.append(operations)
+    return new_operations_list
 
 
-def get_executed_operations(operations_list):
-    """Функция показывает данные об успешных операциях"""
-    executed_list = []
-    for operation in operations_list:
-        if operation.state == "EXECUTED":
-            executed_list.append(operation)
-    return executed_list
+def get_format_date(date):
+    """Форматируем нашу дату в требуемую"""
+    return date.strftime('%d.%m.%Y')
 
 
-def formation_datetime(date):
-    """Метод для форматирования времени из нашего файла json в требуемый"""
-    date_object = datetime.fromisoformat(date)
-    str_dt_obj = date_object.strftime('%d.%m.%y')
-    return str_dt_obj
-
-def card_number_split_and_hide(card_infromation):
+def card_number_split_and_hide(card_information):
     """В этом методе мы разделяем наш счет на название карты и номер для удобной работы с ними, в
     последующих действиях этот метот можно будет применить к from и to. Также мы не трогаем счет, и закрываем
     нужное кол-во цифр звездочками для секретности"""
-    if "Счет" in card_infromation:
-        return "Счет **{}".format(card_infromation[-4:])
+    if "Счет" in card_information:
+        return "Счет **{}".format(card_information[-4:])
     else:
-        card_info = card_infromation.split(" ")
+        card_info = card_information.split(" ")
         card_name = []
         for card in card_info:
             if card.isalpha():
@@ -70,12 +43,13 @@ def card_number_split_and_hide(card_infromation):
         return " ".join(card_name)
 
 
-def get_five_last_executed_operations(operations):
-    """Выводит последние 5 успешных операций"""
-    for operation in operations[0:5]:
-        print(f"""{formation_datetime(operation.date)} {operation.description}
-        {card_number_split_and_hide(operation.by)} -> {card_number_split_and_hide(operation.to)}
-        {operation.operation_amount.amount} {operation.operation_amount.currency.name}""")
-        print("")
+def get_last_five_operations(data):
+    """Сортировка по убыванию дат, оставляем 5 последних операций"""
+    return sorted(data, key=lambda x: x['date'], reverse=True)[:5]
 
 
+def get_amount_and_currency(operation):
+    """Возвращаем стоимость и валюту"""
+    amount = operation['operationAmount']['amount']
+    currency = operation['operationAmount']['currency']['name']
+    return f'{amount} {currency}'
